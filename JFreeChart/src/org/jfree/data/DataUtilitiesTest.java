162cc96e9f4d7d585b87144967969309465ba934
Kaumil Patel <spdmode@gmail.com>
2022-02-03 12:13:21 -0700

    Project Setup

---
+++ JFreeChart/src/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,14 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+import org.jfree.data.DataUtilities;
+import org.junit.*;
+
+class DataUtilitiesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ JFreeChart/src/org/jfree/data/RangeTest.java
@@ -0,0 +1,14 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+import org.jfree.data.Range;
+import org.junit.*;
+
+class RangeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
