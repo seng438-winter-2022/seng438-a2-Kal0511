78205e95a0380af77c20a51775a300ced1335b2a
Kaumil Patel <spdmode@gmail.com>
2022-02-07 12:51:40 -0700

    sample tests

--- JFreeChart/src/org/jfree/data/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/DataUtilitiesTest.java
@@ -1,14 +1,64 @@
 package org.jfree.data;
 
 import static org.junit.Assert.*;
-import org.jfree.data.DataUtilities;
+import org.jmock.*;
 import org.junit.*;
 
-class DataUtilitiesTest {
+public class DataUtilitiesTest {
+	private Mockery mockingContext;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		mockingContext = new Mockery();
 	}
 
+	@Test
+	public void test() {
+
+	}
+
+//	@Test
+//	void calculateColumnTotalTest(){
+//		final DefaultKeyedValues2D values = context.mock(DefaultKeyedValues2D.class);
+//		double result = DataUtilities.calculateColumnTotal(values, 0);
+//	}
+
+	@Test
+	public void calculateColumnTotalForTwoValues() {
+		Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getRowCount();
+				will(returnValue(2));
+				one(values).getColumnCount();
+				will(returnValue(1));
+				one(values).getValue(0, 0);
+				will(returnValue(7.5));
+				one(values).getValue(1, 0);
+				will(returnValue(2.5));
+			}
+		});
+		double result = DataUtilities.calculateColumnTotal(values, 0);
+		assertEquals(result, 10.0, .000000001d);
+	}
+
+	@Test
+	public void cloneTest() {
+		for (int m = 0; m < 10; m++) {
+			for (int n = 0; n < 10; n++) {
+				double[][] array = new double[m][n];
+				for (int i = 0; i < m; i++) {
+					for (int j = 0; j < n; j++) {
+						array[i][j] = i + j * 1.2;
+					}
+				}
+				double[][] result = DataUtilities.clone(array);
+				assertArrayEquals(result, array);
+				if (m > 0 && n > 0) {
+					array[0][0] = -1.55;
+					assertNotEquals(array, result);
+				}
+			}
+		}
+	}
 }
--- JFreeChart/src/org/jfree/data/RangeTest.java
+++ JFreeChart/src/org/jfree/data/RangeTest.java
@@ -1,14 +1,32 @@
 package org.jfree.data;
 
 import static org.junit.Assert.*;
-import org.jfree.data.Range;
 import org.junit.*;
 
-class RangeTest {
+public class RangeTest {
+
+	private Range exampleRange;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		exampleRange = new Range(-1, 1);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void centralValueShouldBeZero() {
+		assertEquals("The central value of -1 and 1 should be 0", 0, exampleRange.getCentralValue(), .000000001d);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
 }
