0084cb4110d2fec25aa556a4d571027abe3d98e7
Kaumil Patel <spdmode@gmail.com>
2022-02-08 16:46:30 -0700

    Test Sample

--- JFreeChart/src/org/jfree/data/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/DataUtilitiesTest.java
@@ -13,34 +13,97 @@ public class DataUtilitiesTest {
 	}
 
 	@Test
-	public void test() {
+	public void createNumberArrayTest() {
+		for (int m = 0; m < 10; m++) {
+			double[] array = new double[m];
+			for (int i = 0; i < m; i++) {
+				array[i] = i;
+			}
+			Number[] result = DataUtilities.createNumberArray(array);
+			assertEquals(array.length, result.length);
+			for (int i = 0; i < m; i++) {
+				assertEquals(array[i], result[i]);
+			}
+		}
+	}
 
+	@Test
+	public void createNumberArray2DTest() {
+		for (int m = 0; m < 10; m++) {
+			for (int n = 0; n < 10; n++) {
+				double[][] array = new double[m][n];
+				for (int i = 0; i < m; i++) {
+					for (int j = 0; j < n; j++) {
+						array[i][j] = i + j;
+					}
+				}
+				Number[][] result = DataUtilities.createNumberArray2D(array);
+				assertEquals(array.length, result.length);
+				for (int i = 0; i < m; i++) {
+					assertEquals(array[i].length, result[i].length);
+					for (int j = 0; j < n; j++) {
+					assertEquals(array[i][j], result[i][j]);
+					}
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void equalTest() {
+		for (int m = 0; m < 10; m++) {
+			for (int n = 0; n < 10; n++) {
+				double[][] array1 = new double[m][n];
+				double[][] array2 = new double[m][n];
+				for (int i = 0; i < m; i++) {
+					for (int j = 0; j < n; j++) {
+						array1[i][j] = i + j;
+						array2[i][j] = i + j;
+					}
+				}
+				assertEquals(DataUtilities.equal(array1, array2), true);
+				if (m > 0 && n > 0) {
+					array1[0][0] = -1.55;
+					assertEquals(DataUtilities.equal(array1, array2), false);
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void getCumulativePercentagesTest(){
+		KeyedValues values = mockingContext.mock(KeyedValues.class);
+		mockingContext.checking(new Expectations() {
+			{
+//				oneOf(values).getRowCount(); will(returnValue(2));
+//				oneOf(values).getColumnCount();
+//				will(returnValue(1));
+//				oneOf(values).getValue(0, 0);
+//				will(returnValue(7.5));
+//				oneOf(values).getValue(1, 0);
+//				will(returnValue(2.5));
+			}
+		});
+		KeyedValues result = DataUtilities.getCumulativePercentages(values);
 	}
 
 //	@Test
-//	void calculateColumnTotalTest(){
-//		final DefaultKeyedValues2D values = context.mock(DefaultKeyedValues2D.class);
+//	public void calculateColumnTotalForTwoValues() {
+//		Values2D values = mockingContext.mock(Values2D.class);
+//		mockingContext.checking(new Expectations() {
+//			{
+//				oneOf(values).getRowCount(); will(returnValue(2));
+////				oneOf(values).getColumnCount();
+////				will(returnValue(1));
+////				oneOf(values).getValue(0, 0);
+////				will(returnValue(7.5));
+////				oneOf(values).getValue(1, 0);
+////				will(returnValue(2.5));
+//			}
+//		});
 //		double result = DataUtilities.calculateColumnTotal(values, 0);
+//		assertEquals(result, 10.0, .000000001d);
 //	}
-
-	@Test
-	public void calculateColumnTotalForTwoValues() {
-		Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getRowCount();
-				will(returnValue(2));
-				one(values).getColumnCount();
-				will(returnValue(1));
-				one(values).getValue(0, 0);
-				will(returnValue(7.5));
-				one(values).getValue(1, 0);
-				will(returnValue(2.5));
-			}
-		});
-		double result = DataUtilities.calculateColumnTotal(values, 0);
-		assertEquals(result, 10.0, .000000001d);
-	}
 
 	@Test
 	public void cloneTest() {
--- JFreeChart/src/org/jfree/data/RangeTest.java
+++ JFreeChart/src/org/jfree/data/RangeTest.java
@@ -1,6 +1,8 @@
 package org.jfree.data;
 
 import static org.junit.Assert.*;
+
+import org.jmock.*;
 import org.junit.*;
 
 public class RangeTest {
@@ -19,6 +21,41 @@ public class RangeTest {
 	@Test
 	public void centralValueShouldBeZero() {
 		assertEquals("The central value of -1 and 1 should be 0", 0, exampleRange.getCentralValue(), .000000001d);
+	}
+
+	@Test
+	public void containsTest() {
+		double low = -11.1;
+		double high = 31.5;
+		Range test = new Range(low, high);
+		assertEquals(test.contains(-13.1), false);
+		assertEquals(test.contains(-11.1), true);
+		assertEquals(test.contains(0), true);
+		assertEquals(test.contains(31.5), true);
+		assertEquals(test.contains(41.5), false);
+	}
+	
+	@Test
+	public void equalsTest() {
+		Range values1 = new Range(-10, 10);
+		Range values2 = new Range(-10, 20);
+		Range values3 = new Range(-20, 10);
+		
+		assertEquals(values1.equals(values1), true);
+		assertEquals(values1.equals(values2), false);
+		assertEquals(values1.equals(values3), false);
+	}
+	
+	@Test
+	public void getLowerBoundTest() {
+		Range values = new Range(-10, 10);
+		assertEquals("Lower bound should be -10", -10, values.getLowerBound(), .000000001d);
+	}
+	
+	@Test
+	public void getUpperBound() {
+		Range values = new Range(-10, 10);
+		assertEquals("Upper bound should be 10", 10, values.getUpperBound(), .000000001d);
 	}
 
 	@After
