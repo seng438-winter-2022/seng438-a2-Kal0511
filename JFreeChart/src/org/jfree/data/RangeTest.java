59ed1ac20b75bced50598b34321150ee8ceaf246
ericrenno <rennoeric@gmail.com>
2022-02-11 01:01:05 -0700

    Added tests for boundary conditions

--- JFreeChart/src/org/jfree/data/RangeTest.java
+++ JFreeChart/src/org/jfree/data/RangeTest.java
@@ -4,183 +4,206 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class RangeTest {
-	
-	private Range constrainRangeTest;
-	private Range containsRangeTest;
 
-	@Before
-	public void setUp() throws Exception {
-		constrainRangeTest = new Range(2, 7);
-		containsRangeTest = new Range(-11.5, 31.5);
-	}
-	
-	
-	// getCentralValue
-	
-	@Test
-	public void centralValueShouldBeNegative() {
-		Range values = new Range(-10, 5);
-		assertEquals("The central value of -10 and 5 should be -2.5", -2.5, values.getCentralValue(), .01d);
-	}
+  private Range constrainRangeTest;
+  private Range containsRangeTest;
 
-	@Test
-	public void centralValueShouldBeZero() {
-		Range values = new Range(-10, 10);
-		assertEquals("The central value of -10 and 10 should be 0", 0, values.getCentralValue(), .01d);
-	}
-	
-	@Test
-	public void centralValueShouldBePositive() {
-		Range values = new Range(-5, 10);
-		assertEquals("The central value of -5 and 10 should be 2.5", 2.5, values.getCentralValue(), .01d);
-	}
-	
-	
-	// contains
+  @Before
+  public void setUp() throws Exception {
+    constrainRangeTest = new Range(2, 7);
+    containsRangeTest = new Range(-11.5, 31.5);
+  }
 
-	@Test
-	public void containsTestForLessThanLowerBound() {
-		assertEquals(containsRangeTest.contains(-13.1), false);
+  // getCentralValue
 
-	}
-	
-	@Test
-	public void containsTestForOnLowerBound() {
-		assertEquals(containsRangeTest.contains(-11.1), true);
-	}
-	
-	@Test
-	public void containsTestForInBetweenBounds() {
-		assertEquals(containsRangeTest.contains(0), true);
-	}
-	
-	@Test
-	public void containsTestForOnUpperBound() {
-		assertEquals(containsRangeTest.contains(31.5), true);
-	}
-	
-	@Test
-	public void containsTestForMoreThanUpperBound() {
-		assertEquals(containsRangeTest.contains(41.5), false);
-	}
-	
-	
-	// equals
+  @Test
+  public void centralValueShouldBeNegative() {
+    Range values = new Range(-10, 5);
+    assertEquals("The central value of -10 and 5 should be -2.5", -2.5, values.getCentralValue(), .01d);
+  }
 
-	@Test
-	public void equalsTestForSameRange() {
-		Range values = new Range(-10, 10);
-		Range results = new Range(-10, 10);
-		assertEquals(values.equals(results), true);
-	}
-	
-	@Test
-	public void equalsTestForLowerRange() {
-		Range values = new Range(-10, 10);
-		Range results = new Range(-20, 10);
-		assertEquals(values.equals(results), false);
-	}
-	
-	@Test
-	public void equalsTestForHigherRange() {
-		Range values = new Range(-10, 10);
-		Range results = new Range(-10, 20);
-		assertEquals(values.equals(results), false);
-	}
-	
-	
-	// getLowerBound
+  @Test
+  public void centralValueShouldBeZero() {
+    Range values = new Range(-10, 10);
+    assertEquals("The central value of -10 and 10 should be 0", 0, values.getCentralValue(), .01d);
+  }
 
-	@Test
-	public void getLowerBoundTest() {
-		Range values = new Range(-10, 10);
-		assertEquals("Lower bound should be -10", -10, values.getLowerBound(), .01d);
-	}
-	
-	
-	// getUpperBound
- 
-	@Test
-	public void getUpperBoundTest() {
-		Range values = new Range(-10, 10);
-		assertEquals("Upper bound should be 10", 10, values.getUpperBound(), .01d);
-	}
-	
+  @Test
+  public void centralValueShouldBePositive() {
+    Range values = new Range(-5, 10);
+    assertEquals("The central value of -5 and 10 should be 2.5", 2.5, values.getCentralValue(), .01d);
+  }
 
-	// combine
-	
-	@Test
-	public void combineTestIntersect() {
-		Range test1, test2;
-		Range result, expected;
+  // contains
 
-		test1 = new Range(0, 10);
-		test2 = new Range(5, 15);
-		expected = new Range(0, 15);
-		result = Range.combine(test1, test2);
-		assertEquals(result, expected);
-	}
+  @Test
+  public void containsTestForLessThanLowerBound() {
+    assertEquals(containsRangeTest.contains(-13.1), false);
 
-	@Test
-	public void combineTestNoOverlap() {
-		Range test1, test2;
-		Range result, expected;
+  }
 
-		test1 = new Range(0, 10);
-		test2 = new Range(15, 20);
-		expected = new Range(0, 20);
-		result = Range.combine(test1, test2);
-		assertEquals(result, expected);
-	}
+  @Test
+  public void containsTestForOnLowerBound() {
+    assertEquals(containsRangeTest.contains(-11.1), true);
+  }
 
-	@Test
-	public void combineTestNull() {
-		Range test1, test2;
-		Range result, expected;
+  @Test
+  public void containsTestForInBetweenBounds() {
+    assertEquals(containsRangeTest.contains(0), true);
+  }
 
-		test1 = null;
-		test2 = new Range(0, 10);
-		expected = new Range(0, 10);
-		result = Range.combine(test1, test2);
-		assertEquals(result, expected);
-	}
-	
+  @Test
+  public void containsTestForOnUpperBound() {
+    assertEquals(containsRangeTest.contains(31.5), true);
+  }
 
-	// constrain
-	
-	@Test
-	public void constrainTestInsideRange() {
-		double result;
-		result = constrainRangeTest.constrain(3);
-		assertEquals(result, 3, 0.01d);
-	}
+  @Test
+  public void containsTestForMoreThanUpperBound() {
+    assertEquals(containsRangeTest.contains(41.5), false);
+  }
 
-	@Test
-	public void constrainTestOutsideRangeAbove() {
-		double result;
-		result = constrainRangeTest.constrain(8);
-		assertEquals(result, 7, 0.01d);
-	}
+  @Test
+  public void containsTestMin() {
+    Range minRange = new Range(0, Double.MIN_VALUE);
+    assertEquals(minRange.contains(Double.MIN_VALUE), true);
+  }
 
-	@Test
-	public void constrainTestOutsideRangeBelow() {
-		double result;
-		result = constrainRangeTest.constrain(1);
-		assertEquals(result, 2, 0.01d);
-	}
+  @Test
+  public void containsTestMax() {
+    Range minRange = new Range(0, Double.MAX_VALUE);
+    assertEquals(minRange.contains(Double.MAX_VALUE), true);
+  }
 
-	@Test
-	public void constrainTestOnLower() {
-		double result;
-		result = constrainRangeTest.constrain(2);
-		assertEquals(result, 2, 0.01d);
-	}
+  // equals
 
-	@Test
-	public void constrainTestOnUpper() {
-		double result;
-		result = constrainRangeTest.constrain(7);
-		assertEquals(result, 7, 0.01d);
-	}
+  @Test
+  public void equalsTestForSameRange() {
+    Range values = new Range(-10, 10);
+    Range results = new Range(-10, 10);
+    assertEquals(values.equals(results), true);
+  }
+
+  @Test
+  public void equalsTestForLowerRange() {
+    Range values = new Range(-10, 10);
+    Range results = new Range(-20, 10);
+    assertEquals(values.equals(results), false);
+  }
+
+  @Test
+  public void equalsTestForHigherRange() {
+    Range values = new Range(-10, 10);
+    Range results = new Range(-10, 20);
+    assertEquals(values.equals(results), false);
+  }
+
+  // getLowerBound
+
+  @Test
+  public void getLowerBoundTest() {
+    Range values = new Range(-10, 10);
+    assertEquals("Lower bound should be -10", -10, values.getLowerBound(), .01d);
+  }
+
+  // getUpperBound
+
+  @Test
+  public void getUpperBoundTest() {
+    Range values = new Range(-10, 10);
+    assertEquals("Upper bound should be 10", 10, values.getUpperBound(), .01d);
+  }
+
+  // combine
+
+  @Test
+  public void combineTestIntersect() {
+    Range test1, test2;
+    Range result, expected;
+
+    test1 = new Range(0, 10);
+    test2 = new Range(5, 15);
+    expected = new Range(0, 15);
+    result = Range.combine(test1, test2);
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void combineTestNoOverlap() {
+    Range test1, test2;
+    Range result, expected;
+
+    test1 = new Range(0, 10);
+    test2 = new Range(15, 20);
+    expected = new Range(0, 20);
+    result = Range.combine(test1, test2);
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void combineTestNull() {
+    Range test1, test2;
+    Range result, expected;
+
+    test1 = null;
+    test2 = new Range(0, 10);
+    expected = new Range(0, 10);
+    result = Range.combine(test1, test2);
+    assertEquals(result, expected);
+  }
+
+  // constrain
+
+  @Test
+  public void constrainTestInsideRange() {
+    double result;
+    result = constrainRangeTest.constrain(3);
+    assertEquals(result, 3, 0.01d);
+  }
+
+  @Test
+  public void constrainTestOutsideRangeAbove() {
+    double result;
+    result = constrainRangeTest.constrain(8);
+    assertEquals(result, 7, 0.01d);
+  }
+
+  @Test
+  public void constrainTestOutsideRangeBelow() {
+    double result;
+    result = constrainRangeTest.constrain(1);
+    assertEquals(result, 2, 0.01d);
+  }
+
+  @Test
+  public void constrainTestOnLower() {
+    double result;
+    result = constrainRangeTest.constrain(2);
+    assertEquals(result, 2, 0.01d);
+  }
+
+  @Test
+  public void constrainTestOnUpper() {
+    double result;
+    result = constrainRangeTest.constrain(7);
+    assertEquals(result, 7, 0.01d);
+  }
+
+  @Test
+  public void constrainTestOnMin() {
+    double result = Double.MIN_VALUE;
+    double precision = 0.00000000000000000000000000000000000000000000001d;
+    Range minRange = new Range(0, Double.MIN_VALUE);
+
+    assertEquals(result, minRange.constrain(result), precision);
+  }
+
+  @Test
+  public void constrainTestOnMax() {
+    double result = Double.MAX_VALUE;
+    double precision = 0.00000000000000000000000000000000000000000000001d;
+    Range minRange = new Range(0, Double.MAX_VALUE);
+    assertEquals(result, minRange.constrain(result), precision);
+
+  }
 
 }
